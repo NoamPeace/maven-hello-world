9af157ce2234a4f417c253ca44443fc5386510ce
NoamPeace <90749068+NoamPeace@users.noreply.github.com>
2022-04-06 17:08:20 +0300

    Update App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,14 +1,14 @@
 package com.mycompany.app;
 
 /**
- * Hello Ilya.G!
+ * Hello World!
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Ilya.G!" );
 
         System.out.println("A NullPointerException is a drag...");
         Object myObject = null;
